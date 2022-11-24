ee912aad0c27f316100351b3c0847537e27079d0
Chris Patti <feoh@feoh.org>
2022-11-23 22:07:29 -0500

    Fixed module structure but server blocks so test can't finish. Duh :)

---
+++ boardgameserver.py
@@ -0,0 +1,56 @@
+import socket
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 8907  # Port to listen on (non-privileged ports are > 1023)
+
+class BoardGameServer:
+
+    def __init__(self, host=HOST, port=PORT):
+
+        self.host = host
+        self.port = port
+
+        self.board_sizes = {
+            'checkers': 8,
+            'go': 19,
+            'othello': 8
+        }
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((hostname, port))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                conn.send(b"Would you like to play a game?\n")
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+
+                    match data.split():
+                        case ["play", game]:
+                            self.register_game(game)
+                        case ["end"]:
+                            self.end_game()
+                        case ["place",x,y]:
+                            self.place(x,y)
+                        case ["remove",x,y]:
+                            remove(x,y)
+
+
+    def register_game(self, game: str):
+        game_state = {
+            'board_size': self.board_sizes[game],
+            'game': game,
+        }
+
+    def end_game():
+        pass
+
+
+def main():
+    bgs = BoardGameServer()
+
+if __name__ == '__main__':
+    main()
