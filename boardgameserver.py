340d38f6d5f21d5dc3c95148f42ff9cc3b802974
Chris Patti <feoh@feoh.org>
2022-11-24 19:48:18 -0500

    Refactor to more easily support multiprocessing.

--- boardgameserver.py
+++ boardgameserver.py
@@ -16,8 +16,10 @@ class BoardGameServer:
             'othello': 8
         }
 
+
+    def create_server(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((hostname, port))
+            s.bind((self.host, self.port))
             s.listen()
             conn, addr = s.accept()
             with conn:
@@ -28,15 +30,21 @@ class BoardGameServer:
                     if not data:
                         break
 
-                    match data.split():
-                        case ["play", game]:
-                            self.register_game(game)
-                        case ["end"]:
-                            self.end_game()
-                        case ["place",x,y]:
-                            self.place(x,y)
-                        case ["remove",x,y]:
-                            remove(x,y)
+                    self.parse_command(data)
+
+
+    def parse_command(self, data):
+        match data.split():
+            case ["play", game]:
+                self.register_game(game)
+            case ["end"]:
+                self.end_game()
+            case ["place",x,y]:
+                self.place_piece(x,y)
+            case ["remove",x,y]:
+                self.remove_piece(x,y)
+
+
 
 
     def register_game(self, game: str):
@@ -45,12 +53,19 @@ class BoardGameServer:
             'game': game,
         }
 
-    def end_game():
+    def end_game(self):
+        pass
+
+    def place_piece(self, x,y):
+        pass
+
+    def remove_piece(self):
         pass
 
 
 def main():
     bgs = BoardGameServer()
+    bgs.create_server()
 
 if __name__ == '__main__':
     main()
